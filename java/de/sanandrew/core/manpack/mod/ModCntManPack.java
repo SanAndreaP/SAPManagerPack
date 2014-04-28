3a5c90c193fb4951709e375af2d30548266d9f8e
SanAndreasP <1294@live.de>
2014-04-28 08:01:08 +0200

    little cleanup

--- java/de/sanandrew/core/manpack/mod/ModCntManPack.java
+++ java/de/sanandrew/core/manpack/mod/ModCntManPack.java
@@ -19,22 +19,24 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 import de.sanandrew.core.manpack.mod.packet.ChannelHandler;
 
-public class ModCntManPack extends DummyModContainer
+public class ModCntManPack
+    extends DummyModContainer
 {
-	public static final String MOD_VERSION = "1.6.4-2.0.0";
-	public static final String MOD_CHANNEL = "sapmanpack";
-	public static final String MOD_ID = "sapmanpack";
-	public static final String MOD_LOG = "SAPManPack";
+    public static ChannelHandler MANPACK_CH_HANDLER;
+    public static final String MOD_CHANNEL = "sapmanpack";
+    public static final String MOD_ID = "sapmanpack";
+    public static final String MOD_LOG = "SAPManPack";
 
-	public static ChannelHandler MANPACK_CH_HANDLER;
+    public static final String MOD_VERSION = "1.6.4-2.0.0";
 
-	public static File modLocation;
+    public static File modLocation;
+
+    @SidedProxy(clientSide = "de.sanandrew.core.manpack.mod.client.ClientProxy", serverSide = "de.sanandrew.core.manpack.mod.CommonProxy")
+    public static CommonProxy proxy;
+
     private ModMetadata md;
 
-	@SidedProxy(clientSide="de.sanandrew.core.manpack.mod.client.ClientProxy", serverSide="de.sanandrew.core.manpack.mod.CommonProxy")
-	public static CommonProxy proxy;
-
-	public ModCntManPack() {
+    public ModCntManPack() {
         super(new ModMetadata());
         this.md = super.getMetadata();
         File mcmod = new File(ModCntManPack.modLocation, "mcmod.info");
@@ -47,42 +49,22 @@ public class ModCntManPack extends DummyModContainer
         }
 
         if( mc != null ) {
-        	this.md = mc.getMetadataForId(ModCntManPack.MOD_ID, null);
+            this.md = mc.getMetadataForId(ModCntManPack.MOD_ID, null);
         } else {
-        	this.md.authorList = Arrays.asList(new String[] { "SanAndreasP" });
-        	this.md.description = "A helper coremod which is needed for all my mods.";
-        	this.md.modId = ModCntManPack.MOD_ID;
-        	this.md.version = ModCntManPack.MOD_VERSION;
-        	this.md.name = "SanAndreasPs Manager Pack CORE edition";
-        	this.md.url = "http://www.minecraftforge.net/forum/index.php/topic,2828.0.html";
+            this.md.authorList = Arrays.asList(new String[] { "SanAndreasP" });
+            this.md.description = "A helper coremod which is needed for all my mods.";
+            this.md.modId = ModCntManPack.MOD_ID;
+            this.md.version = ModCntManPack.MOD_VERSION;
+            this.md.name = "SanAndreasPs Manager Pack CORE edition";
+            this.md.url = "http://www.minecraftforge.net/forum/index.php/topic,2828.0.html";
         }
         MANPACK_CH_HANDLER = new ChannelHandler(ModCntManPack.MOD_ID, ModCntManPack.MOD_CHANNEL);
-	}
+    }
 
-	@Override
-	public boolean registerBus(EventBus bus, LoadController controller) {
-		bus.register(this);
-		return true;
-	}
-
-	@Subscribe
-	public void preInit(FMLPreInitializationEvent evt) {
-		ModCntManPack.proxy.registerPackets();
-//		MANPACK_CH_HANDLER.registerPacket(ExamplePacket.class);
-	}
-
-	@Subscribe
-	public void init(FMLInitializationEvent evt) {
-	    MANPACK_CH_HANDLER.initialise();
-	    FMLCommonHandler.instance().bus().register(new TickEventUpdMgr());
-		ModCntManPack.proxy.registerRenderStuff();
-	}
-
-	@Subscribe
-    public void postInit(FMLPostInitializationEvent evt) {
-        MANPACK_CH_HANDLER.postInitialise();
-	}
-
+    @Override
+    public String getDisplayVersion() {
+        return this.md.version;
+    }
 
     @Override
     public ModMetadata getMetadata() {
@@ -104,13 +86,32 @@ public class ModCntManPack extends DummyModContainer
         return this.md.version;
     }
 
+    @Subscribe
+    public void init(FMLInitializationEvent evt) {
+        MANPACK_CH_HANDLER.initialise();
+        FMLCommonHandler.instance().bus().register(new TickEventUpdMgr());
+        ModCntManPack.proxy.registerRenderStuff();
+    }
+
+    @Subscribe
+    public void postInit(FMLPostInitializationEvent evt) {
+        MANPACK_CH_HANDLER.postInitialise();
+    }
+
+    @Subscribe
+    public void preInit(FMLPreInitializationEvent evt) {
+        ModCntManPack.proxy.registerPackets();
+        // MANPACK_CH_HANDLER.registerPacket(ExamplePacket.class);
+    }
+
     @Override
-    public String getDisplayVersion() {
-        return this.md.version;
+    public boolean registerBus(EventBus bus, LoadController controller) {
+        bus.register(this);
+        return true;
     }
 
     @Override
     public String toString() {
-        return this.md != null ? this.getModId() : "Container ("+ModCntManPack.MOD_ID+") @" + System.identityHashCode(this);
+        return this.md != null ? this.getModId() : "Container (" + ModCntManPack.MOD_ID + ") @" + System.identityHashCode(this);
     }
 }
